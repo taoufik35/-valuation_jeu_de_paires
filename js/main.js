4cfcd0160e9c8e5024a35337b4d2ce02dea3f1af
unknown <aitdra.taoufik@gmail.com>
2021-04-14 17:08:30 +0200

    add picture card

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let red = document.getElementById("red","red1");
+
+
+
+change();
+
+function change(){
+    red.addEventListener("click", function(){
+        red.style.background = "black";
+    })
+}
+
+
